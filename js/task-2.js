4513e7af6a517124b5b163072c8d65d73eb1bb0c
dima20d22 <dimasurovatka22@gmail.com>
2024-11-26 11:15:23 +0100

    module 6 starts

---
+++ js/task-2.js
@@ -0,0 +1,27 @@
+class storage {
+  items = [];
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(newItem) {
+    this.newItem = newItem;
+  }
+
+  removeItem(itemToRemove) {
+    this.itemToRemove = itemToRemove;
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
